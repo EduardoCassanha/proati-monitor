d6ca64c2a1efb7021988b6b47c8b153da3ce5a7d
EduardoCassanha <eduardocassanhaperalta@gmail.com>
2026-05-20 12:18:37 -0300

    chore(deploy): upgrade install.bat to use Task Scheduler and SYSTEM privileges

--- agent/install.bat
+++ agent/install.bat
@@ -1,5 +1,22 @@
 @echo off
-echo installing PROATI Monitor Agent...
-copy "%~dp0agent.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\agent.exe"
-copy "%~dp0config.ini" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\config.ini"
-echo done! Agent will start automatically with Windows.
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Please right-click and select "Run as Administrator".
+    pause
+    exit /b
+)
+
+echo [+] Installing PROATI Monitor Agent...
+
+if not exist "C:\Program Files\PROATI-Monitor" (
+    mkdir "C:\Program Files\PROATI-Monitor"
+)
+
+copy /y "%~dp0WinNetMonitor.exe" "C:\Program Files\PROATI-Monitor\" >nul
+copy /y "%~dp0config.ini" "C:\Program Files\PROATI-Monitor\" >nul
+
+schtasks /create /tn "PROATI_Agent" /tr "'C:\Program Files\PROATI-Monitor\WinNetMonitor.exe'" /sc onstart /ru "SYSTEM" /f >nul
+
+echo.
+echo [OK] Agent installed successfully and configured in Task Scheduler!
+timeout /t 3 >nul
