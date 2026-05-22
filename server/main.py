efaf295d35e97e5f99a5ea4fa6eb08da60f81b60
EduardoCassanha <eduardocassanhaperalta@gmail.com>
2026-05-22 08:48:04 -0300

    fix(server): pass app object directly to uvicorn to resolve pyinstaller import error

--- server/main.py
+++ server/main.py
@@ -181,4 +181,4 @@ def get_events(db: Session = Depends(get_db)):
     ]
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
